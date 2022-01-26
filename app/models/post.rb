4cb41b9aa23971c8bc8aab0eaa8a0054f1005f29
PavelGritsenko <pasha.zp.ua94@gmail.com>
2022-01-26 23:14:13 +0200

    callback in model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ApplicationRecord
 
   has_rich_text :content
 
-
-  scope :past_week, -> { where(created_at: 1.week.ago.beginning_of_week..1.week.ago.end_of_week) }
+  scope :past_week, -> { where(created_at: Time.zone.now.at_beginning_of_week..Time.zone.now.at_end_of_week)}
+  # scope :past_week, -> { where(created_at: 1.week.ago.beginning_of_week..1.week.ago.end_of_week) }
   scope :desc, -> { order(created_at: :desc) }
 end
  
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
 
   has_many :comments
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true
+
+  before_validation :ensure_name_has_a_value
+
+  private
+    def ensure_name_has_a_value
+      if name.blank?
+        self.name = email.split('@')[0] unless email.blank?
+      end
+    end
 end
